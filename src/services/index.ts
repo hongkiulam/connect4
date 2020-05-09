69ad8d87725791f3c794c5f466bb95ec4a86942b
Haydon Lam <lamhaydon@gmail.com>
2020-05-09 14:28:18 +0100

    Using just socket io

--- src/services/index.ts
+++
@@ -1,9 +0,0 @@
-import { Application } from '../declarations';
-import moves from './moves/moves.service';
-import rooms from './rooms/rooms.service';
-// Don't remove this comment. It's needed to format import lines nicely.
-
-export default function (app: Application) {
-  app.configure(moves);
-  app.configure(rooms);
-}
